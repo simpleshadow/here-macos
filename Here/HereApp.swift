6ea9db8cf683b33ac54f49d63f050599eee83856
Corey Gwin <coreygwin@gmail.com>
2022-08-23 12:47:02 -0700

    Initial Commit

---
+++ Here/HereApp.swift
@@ -0,0 +1,17 @@
+//
+//  HereApp.swift
+//  Here
+//
+//  Created by Simple on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct HereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
